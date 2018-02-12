808ebafd299cfbda295d9beeb00dff9cddf40180
Takuya Tominaga <tom.qq.emt.03tw@gmail.com>
2018-02-12 18:29:24 +0900

    Devise/Rails_Adminの導入

--- Gemfile
+++ Gemfile
@@ -1,5 +1,9 @@
 source 'https://rubygems.org'
 
+gem 'devise'
+gem 'rails_admin'
+gem 'cancancan'
+
 git_source(:github) do |repo_name|
   repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?("/")
   "https://github.com/#{repo_name}.git"
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'medical_app/home'
 
-  get 'medical_app/search'
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  devise_for :admin_users
+  
+  root to: "medical_app#home"
+  match '/search',  to: 'medical_app#search', via: 'get'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
