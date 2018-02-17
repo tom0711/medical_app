93daea4b5ace65d82397e7eed778f3d4e5c25380
Takuya Tominaga <tom.qq.emt.03tw@gmail.com>
2018-02-18 05:05:56 +0900

    Gem ancestry 削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180212193313) do
+ActiveRecord::Schema.define(version: 20180217195612) do
 
   create_table "categories", force: :cascade do |t|
-    t.string   "category_name"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
