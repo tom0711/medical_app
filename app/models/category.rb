1ee4ffe2cce57f52dd3aa35914a644e855beb086
Takuya Tominaga <tom.qq.emt.03tw@gmail.com>
2018-02-25 17:32:53 +0900

    階層Categoryの設置

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,11 @@
 class Category < ApplicationRecord
+  has_many :children, class_name: "Category", foreign_key: "parent_category_id"
+  belongs_to :parent, class_name: "Category", foreign_key: "parent_category_id"
+  # class Category < ActiveRecord
+  #   has_many :children, class_name: 'Category',
+  #            foreign_key: 'parent_id',
+  # end
+  #
+  # category = Category.find_by(id: 1)
+  # category.children
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180217195612) do
+ActiveRecord::Schema.define(version: 20180225074808) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
+    t.integer  "parent_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
